69bd58e7d6591dfdc756522d4bffe70240e4ff40
Kendrick Lewis <lewiskendrick60@gmail.com>
2024-01-15 16:19:23 -0600

    Set up

---
+++ CardsOfOpprotunity/CardsOfOpprotunity/CardsOfOpprotunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsOfOpprotunityApp.swift
+//  CardsOfOpprotunity
+//
+//  Created by kendrick lewis on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsOfOpprotunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
