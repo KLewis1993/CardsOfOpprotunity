328b04658698f5182b47e46900a5cc3ff8b3f228
Kendrick Lewis <lewiskendrick60@gmail.com>
2024-01-20 16:10:56 -0600

    Adding oddsview and gamedata back into project

---
+++ CardsOfOpprotunity/CardsOfOpprotunity/Views/StartingPages/GameDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  GameDataModel.swift
+//  CardsOfOpprotunity
+//
+//  Created by kendrick lewis on 1/20/24.
+//
+
+import Foundation
+
+class GameData: ObservableObject {
+    @Published var playerOneName: String = ""
+    @Published var playerTwoName: String = ""
+    @Published var enteredBet: String = ""
+    
+    var isBothNamesEntered: Bool {
+        !playerOneName.isEmpty && !playerTwoName.isEmpty && !enteredBet.isEmpty
+    }
+}
